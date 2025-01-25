e8c1e224bc894f369d9c3ec05d767d43b09a4cd1
Dev <devitaliya22@gmail.com>
2025-01-25 14:32:46 +0530

    Eslint error slved on build

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,25 +1,26 @@
+/* eslint-disable no-var */
+
 import { drizzle } from 'drizzle-orm/neon-http';
 import { env } from '@/lib/env';
 
-if (!env.DATABASE_URL) {
+if (!env.DATABASE_URL || env.DATABASE_URL.trim() === '') {
   throw new Error('DATABASE_URL is not set');
 }
 
 const createDrizzleInstance = () => {
-    console.log("Db intialized");
-    return drizzle(env.DATABASE_URL!,{
-      logger:true
-    })
+  console.log("Db initialized");
+  return drizzle(env.DATABASE_URL, { logger: true });
 };
+
 
 declare global {
   var _db: ReturnType<typeof drizzle> | undefined;
 }
 
-const db = global._db ?? createDrizzleInstance();
-
-if (process.env.NODE_ENV !== 'production') {
-  global._db = db;
+if (typeof global._db === 'undefined') {
+  global._db = createDrizzleInstance();
 }
+
+const db = global._db;
 
 export default db;
