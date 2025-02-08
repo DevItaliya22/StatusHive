6d80d2bbabd292675645061eaa780d810c89e06e
Dev <devitaliya22@gmail.com>
2025-02-08 22:08:36 +0530

    Trying edge

---
+++ src/app/api/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from "next/server";
+
+export const config = {
+    runtime: 'edge', // Make this an edge function
+  };
+  
+  export default async function handler(req:NextRequest) {
+    const locationHeader = req.headers.get('X-Vercel-Location');
+    
+    // Log the location for debugging
+    console.log('Request handled by edge location:', locationHeader);
+  
+    const response = await fetch('https://devitaliya.me', {
+      method: 'GET',
+      headers: {
+        'X-Vercel-Location': locationHeader || 'auto', // Send the location header
+      },
+    });
+  
+    const data = await response.json();
+  
+    return new Response(JSON.stringify(data), {
+      status: response.status,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  
