fc3bfd3aa781bc9319467711b227410a790b6fad
Dev <devitaliya22@gmail.com>
2025-02-09 11:47:38 +0530

    Trying to solve internal server error on prod

--- src/app/api/edge/route.ts
+++ src/app/api/edge/route.ts
@@ -1,26 +1,24 @@
 import { NextRequest } from "next/server";
 
-
-export const runtime = 'edge'
+export const runtime = 'edge';
 export const preferredRegion = ["arn1"];
 export const dynamic = 'force-dynamic';
 
-  export async function GET(req:NextRequest) {
-    const locationHeader = req.headers.get('X-Vercel-Location');
-    
-    // Log the location for debugging
-    console.log('Request handled by edge location:', locationHeader);
-  
-    const response = await fetch('https://devitaliya.me');
-  
-    const data = await response.json();
-  
-    return new Response(JSON.stringify(data), {
-      status: response.status,
-      headers: {
-        'Content-Type': 'application/json',
-        "Location-Header": locationHeader || "No location header",
-      },
-    });
-  }
-  
+export async function GET(req: NextRequest) {
+  const locationHeader = req.headers.get('X-Vercel-Location');
+
+  console.log('Request handled by edge location:', locationHeader);
+
+  const response = await fetch('https://devitaliya.me');
+  const data = await response.text(); // Convert response to string
+
+  return new Response(JSON.stringify({ 
+    data, 
+    locationHeader: locationHeader || "No location header",
+  }), {
+    status: response.status,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
