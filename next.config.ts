0dd9822b4c162ca8ae44be9a6654cd2448a20800
Dev <devitaliya22@gmail.com>
2025-02-09 11:23:04 +0530

    IDk why am i commit ing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig :NextConfig= {
+  async headers() {
+      return [
+          {
+              // matching all API routes
+              source: "/api/:path*",
+              headers: [
+                 // omitted for brevity...
+              ]
+          },
+          {
+              source: "/api/special-data",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "false" },
+                  { key: "Access-Control-Allow-Origin", value: "https://example.com" },
+                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                  { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+              ]
+          }
+      ]
+  }
+}
 
 export default nextConfig;
