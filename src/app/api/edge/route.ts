b76b92908ce269b3795596de3d65e6d0d94d64de
Dev <devitaliya22@gmail.com>
2025-02-09 11:41:06 +0530

    Location ?

--- src/app/api/edge/route.ts
+++ src/app/api/edge/route.ts
@@ -1,21 +1,17 @@
 import { NextRequest } from "next/server";
 
-export const config = {
-    runtime: 'edge', // Make this an edge function
-  };
-  
+
+export const runtime = 'edge'
+export const preferredRegion = ["arn1"];
+export const dynamic = 'force-dynamic';
+
   export async function GET(req:NextRequest) {
     const locationHeader = req.headers.get('X-Vercel-Location');
     
     // Log the location for debugging
     console.log('Request handled by edge location:', locationHeader);
   
-    const response = await fetch('https://devitaliya.me', {
-      method: 'GET',
-      headers: {
-        'X-Vercel-Location': locationHeader || 'auto', // Send the location header
-      },
-    });
+    const response = await fetch('https://devitaliya.me');
   
     const data = await response.json();
   
@@ -23,6 +19,7 @@ export const config = {
       status: response.status,
       headers: {
         'Content-Type': 'application/json',
+        "Location-Header": locationHeader || "No location header",
       },
     });
   }
